f757b27c0bde9861b7cc6203ef8430f4d343310a
nightmare199x <79034221+nightmare199x@users.noreply.github.com>
2026-07-16 16:45:48 +0700

    Update [[path]].js

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -1,6 +1,6 @@
 // ==================== CONFIG ====================
-const UPSTREAM_PRIMARY = 'https://bu0eg1tdzu.cloudflare-gateway.com/dns-query';
-const UPSTREAM_FALLBACK = 'https://rhpcv957tj.cloudflare-gateway.com/dns-query';
+const UPSTREAM_PRIMARY = 'https://fjmxy8q330.cloudflare-gateway.com/dns-query';
+const UPSTREAM_FALLBACK = 'https://bo7xp0zb52.cloudflare-gateway.com/dns-query';
 const UPSTREAM_GEO_BYPASS = 'https://dns.mullvad.net/dns-query'; // Re-resolve without ECS when geo-block returns loopback
 const UPSTREAM_TIMEOUT = 5000;
 
